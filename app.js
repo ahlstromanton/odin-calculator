58cba7c8afc44695eadfb4a745d50aac4aa3e28e
anton <anton.l.ahlstrom@gmail.com>
2023-09-18 21:03:16 +0200

    removed bootstrap, started user input functions

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let firstNumber = 0;
+let secondNumber = 0;
+
 function add() {
     return a + b;
 }
@@ -13,4 +16,30 @@ function multiply() {
 function divide() {
     return a / b;
 }
+
+
+function getUserInput() {
+
+}
+
+// Get the first input as a array of strings?, i.e. "1" "1" "1" "1" = 1111
+function firstNumberConverter() {
+    let firstNumberArray = [];
+    while (firstNumberArray.length <= 5) {
+        let number = document.querySelectorAll('.number'); //Osäker på denna
+        firstNumberArray.push(number);
+    }
+    let firstNumberString = firstNumberArray.toString();
+    let firstNumber = parseInt(firstNumberString);
+    return firstNumber;
+}
+// Maybe needed to concatinate to a string? I.e. "1" + "1" + "1" + "1" > "1111"
+
+// Then convert to number: "1111" > 1111
+
+// Assign this to a variable (a)
+
+// Then do the same for the second variable
+
+// Then run the appropriate function with the two variables assigned above
 
